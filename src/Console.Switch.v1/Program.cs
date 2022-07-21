8dae39fd1d9dae769888fe8152091806d9655de7
apulito <alberto.pulito@gmail.com>
2022-07-21 12:37:36 +0200

    Add basic skeleton of v1 comparison solution

---
+++ src/Console.Switch.v1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Console.Switch.v1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            System.Console.WriteLine("Hello, World!");
+        }
+    }
+}
