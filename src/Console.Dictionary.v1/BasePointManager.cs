35e9d0bf5937e9622efab34ecf6af297893eafe4
apulito <alberto.pulito@gmail.com>
2022-07-21 12:51:45 +0200

    Add ifelse console proj and domain proj

---
+++ src/Console.Dictionary.v1/BasePointManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console.Dictionary.v1
+{
+    internal class BasePointManager
+    {
+    }
+}
---
+++ src/Console.Switch.v1/BasePointManager.cs
@@ -0,0 +1,43 @@
+﻿using Domain.v1;
+
+namespace Console.Switch.v1
+{
+    internal class BasePointManager
+    {
+        /// <summary>
+        /// Use switch statements
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static int GetHitPoint_with_switch(PokemonType value)
+        {
+            var point = 0;
+
+            switch (value)
+            {
+                case PokemonType.NORMAL:
+                    point = 1;
+                    break;
+                case PokemonType.GRASS:
+                    point = 2;
+                    break;
+                case PokemonType.ELECTRIC:
+                    point = 3;
+                    break;
+                case PokemonType.WATER:
+                    point = 4;
+                    break;
+                case PokemonType.FIRE:
+                    point = 5;
+                    break;
+                case PokemonType.BUG:
+                    point = 6;
+                    break;
+
+                // and soo on
+            }
+
+            return point;
+        }
+    }
+}
